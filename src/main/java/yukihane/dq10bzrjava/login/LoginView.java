d2af727d2e716251d191e09e833256fccea20817
yukihane <yukihane.feather@gmail.com>
2015-09-20 17:57:25 +0900

    ログイン画面表示

--- src/main/java/yukihane/dq10bzrjava/login/LoginView.java
+++ src/main/java/yukihane/dq10bzrjava/login/LoginView.java
@@ -3,17 +3,9 @@ package yukihane.dq10bzrjava.login;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class LoginView implements FxmlView<LoginViewModel>, Initializable {
@@ -24,45 +16,10 @@ public class LoginView implements FxmlView<LoginViewModel>, Initializable {
     @FXML
     private WebView webView;
 
-    @FXML
-    private TextField urlField;
-
-    @FXML
-    private Button openButton;
-
-    private WebEngine engine;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println(location);
-
-        engine = webView.getEngine();
-        Worker<Void> worker = engine.getLoadWorker();
-
-        worker.stateProperty().addListener(
-                (ObservableValue<? extends Worker.State> observable,
-                        Worker.State oldValue, Worker.State newValue) -> {
-                    if (newValue == Worker.State.SUCCEEDED) {
-                        String url = engine.getLocation();
-                        if (url != null && !url.isEmpty()) {
-                            urlField.setText(url);
-                        }
-                    }
-                });
-
-        EventHandler<ActionEvent> handler = (event) -> {
-            loadUrl();
-        };
-
-        urlField.setOnAction(handler);
-        openButton.setOnAction(handler);
-    }
-
-    private void loadUrl() {
-        String url = urlField.getText();
-        if (Objects.nonNull(url) && !url.trim().isEmpty()) {
-            engine.load(url);
-        }
+        viewModel.initialize(webView.getEngine());
+        viewModel.loadLoginPage();
     }
 
 }
--- src/main/java/yukihane/dq10bzrjava/login/LoginViewModel.java
+++ src/main/java/yukihane/dq10bzrjava/login/LoginViewModel.java
@@ -1,6 +1,21 @@
 package yukihane.dq10bzrjava.login;
 
 import de.saxsys.mvvmfx.ViewModel;
+import java.util.Objects;
+import javafx.scene.web.WebEngine;
 
 public class LoginViewModel implements ViewModel {
+
+    private static final String LOGIN_URL = "https://secure.square-enix.com/oauth/oa/oauthlogin?client_id=happy&redirect_uri=https%3A%2F%2Fhappy.dqx.jp%2Fcapi%2Flogin%2Fsecurelogin%2F&response_type=code&yl=1";
+
+    private WebEngine engine;
+
+    void initialize(WebEngine engine) {
+        this.engine = Objects.requireNonNull(engine);
+    }
+
+    public void loadLoginPage() {
+        engine.load(LOGIN_URL);
+    }
+
 }
