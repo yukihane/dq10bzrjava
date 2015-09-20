52d2e0d950ffe8dca9c2cb3f1b000bf31d292814
yukihane <yukihane.feather@gmail.com>
2015-09-20 20:30:00 +0900

    通知用の定数

---
+++ src/main/java/yukihane/dq10bzrjava/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yukihane.dq10bzrjava;
+
+/**
+ *
+ * @author yuki
+ */
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String LOGIN_COMPLETED = "LOGIN_COMPLETED";
+}
