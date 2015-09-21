4b78bd5b98a8e8862b42de9c36697f6bd3a5c876
yukihane <yukihane.feather@gmail.com>
2015-09-21 19:00:39 +0900

    ログイン画面からの完了通知を受けてキャラクタ名を画面に表示する

--- src/main/java/yukihane/dq10bzrjava/main/MainView.java
+++ src/main/java/yukihane/dq10bzrjava/main/MainView.java
@@ -8,6 +8,12 @@ import javafx.scene.control.Label;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
+import yukihane.dq10don.communication.dto.login.CharacterList;
 
 public class MainView implements FxmlView<MainViewModel>, Initializable {
 
@@ -18,12 +24,44 @@ public class MainView implements FxmlView<MainViewModel>, Initializable {
     private Label idLabel;
 
     @FXML
-    private Label characterLabel;
+    private ComboBox<CharacterList> characterComboBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         idLabel.textProperty().bind(viewModel.idProperty());
-        characterLabel.textProperty().bind(viewModel.characterNameProperty());
+        characterComboBox.itemsProperty().bind(viewModel.getCharactersProperty());
+        characterComboBox.setCellFactory(new Callback<ListView<CharacterList>, ListCell<CharacterList>>() {
+            @Override
+            public ListCell<CharacterList> call(ListView<CharacterList> param) {
+                return new ListCell<CharacterList>() {
+                    @Override
+                    protected void updateItem(CharacterList item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item == null || empty) {
+                            setGraphic(null);
+                        } else {
+                            setText(item.getCharacterName()
+                                    + " (" + item.getSmileUniqueNo() + ")");
+                        }
+                    }
+                };
+            }
+        });
+        characterComboBox.setConverter(new StringConverter<CharacterList>() {
+
+            @Override
+            public String toString(CharacterList object) {
+                if (object == null) {
+                    return null;
+                }
+                return object.getCharacterName();
+            }
+
+            @Override
+            public CharacterList fromString(String string) {
+                return null;
+            }
+        });
     }
 
     @FXML
--- src/main/java/yukihane/dq10bzrjava/main/MainViewModel.java
+++ src/main/java/yukihane/dq10bzrjava/main/MainViewModel.java
@@ -1,27 +1,38 @@
 package yukihane.dq10bzrjava.main;
 
 import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.MvvmFX;
 import de.saxsys.mvvmfx.ViewModel;
 import de.saxsys.mvvmfx.ViewTuple;
 import de.saxsys.mvvmfx.utils.commands.Action;
 import de.saxsys.mvvmfx.utils.commands.Command;
 import de.saxsys.mvvmfx.utils.commands.DelegateCommand;
+import de.saxsys.mvvmfx.utils.notifications.NotificationObserver;
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.property.ReadOnlyListWrapper;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import yukihane.dq10bzrjava.Constants;
 import yukihane.dq10bzrjava.login.LoginView;
+import yukihane.dq10don.communication.dto.login.CharacterList;
+import yukihane.dq10don.communication.dto.login.LoginDto;
 
 public class MainViewModel implements ViewModel {
 
     private final ReadOnlyStringWrapper id = new ReadOnlyStringWrapper("");
-    private final ReadOnlyStringWrapper characterName = new ReadOnlyStringWrapper("");
+    private final ReadOnlyListWrapper<CharacterList> characters;
 
     private final Command loginCommand;
 
     public MainViewModel() {
+        ObservableList<CharacterList> charalist = FXCollections.observableArrayList();
+        characters = new ReadOnlyListWrapper<>(charalist);
         loginCommand = new DelegateCommand(() -> new Action() {
             @Override
             protected void action() throws Exception {
@@ -38,12 +49,12 @@ public class MainViewModel implements ViewModel {
         return id.get();
     }
 
-    public ReadOnlyStringProperty characterNameProperty() {
-        return characterName.getReadOnlyProperty();
+    public ReadOnlyListProperty<CharacterList> getCharactersProperty() {
+        return characters;
     }
 
-    public String getCharacterName() {
-        return characterName.get();
+    public ObservableList<CharacterList> getCharacters() {
+        return characters.get();
     }
 
     public Command getLoginCommand() {
@@ -59,6 +70,18 @@ public class MainViewModel implements ViewModel {
         stage.setScene(scene);
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.show();
+
+        NotificationObserver observer = (String string, Object... os) -> {
+            System.out.println("message received");
+            LoginDto res = (LoginDto) os[0];
+            System.out.println("res: " + res);
+            characters.addAll(res.getCharacterList());
+        };
+
+        MvvmFX.getNotificationCenter().subscribe(
+                viewTuple.getViewModel(),
+                Constants.LOGIN_COMPLETED,
+                observer);
     }
 
 }
