b5dc6c17bca5e3a1f5aa87ecd5b006d76929a6a4
yukihane <yukihane.feather@gmail.com>
2015-09-22 14:53:38 +0900

    デフォルトコメント削除

--- src/main/java/yukihane/dq10bzrjava/login/Starter.java
+++ src/main/java/yukihane/dq10bzrjava/login/Starter.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package yukihane.dq10bzrjava.login;
 
 import de.saxsys.mvvmfx.FluentViewLoader;
@@ -11,8 +6,6 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import yukihane.dq10bzrjava.login.LoginView;
-import yukihane.dq10bzrjava.login.LoginViewModel;
 
 /**
  *
